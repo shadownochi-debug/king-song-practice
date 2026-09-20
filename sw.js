1230dbfce9f951f8925f376a1ec382bbef87a580
shadownochi-debug <shadownochi@gmail.com>
2026-09-20 18:42:12 +0800

    Publish King Song practice app

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='king-song-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./data.json','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
